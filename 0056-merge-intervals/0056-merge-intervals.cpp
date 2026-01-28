0d55ab2f720a6f2da551cb206be43336f82ca59c
Ansh Tayal <tayalansh176@gmail.com>
2026-01-28 11:07:34 +0530

    Time: 2 ms (92.64%), Space: 23.9 MB (58.96%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.cpp
+++ 0056-merge-intervals/0056-merge-intervals.cpp
@@ -8,13 +8,14 @@ public:
         sort(intervals.begin(), intervals.end());
         vector<int>prev=intervals[0];
         for(int i=1;i<n;i++){
-            vector<int>interval= intervals[i];
-            if(interval[0]<=prev[1]){
-                prev[1]=max(interval[1],prev[1]);
+            
+            if(intervals[i][0]<=prev[1]){
+                prev[1]=max(intervals[i][1],prev[1]);
             }
             else{
                 res.push_back(prev);
-                prev=interval;
+                prev[0]=intervals[i][0];
+                prev[1]=intervals[i][1];
             }
         }
         res.push_back(prev);
