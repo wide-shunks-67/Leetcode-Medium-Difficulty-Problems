1a0be5d60baf9afc83771fb14ffa0f2ae6ae7aaa
Ansh Tayal <tayalansh176@gmail.com>
2026-05-23 08:05:53 +0530

    Time: 0 ms (100%), Space: 8.4 MB (85.48%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.cpp
+++ 0069-sqrtx/0069-sqrtx.cpp
@@ -3,23 +3,14 @@ public:
     int mySqrt(int x) {
         if (x == 0 || x == 1)
             return x;
-
-        int start=1;
-        int end=x;
-        int mid=-1;
-         while(start<=end){
-            mid= start + (end - start) /2;
-            long long sq= static_cast<long long>(mid) * mid;
-            if(sq>x){
-                end= mid-1;
-            }
-            else if(sq==x){
-                return mid;
-            }
-            else{
-                start= mid + 1;
-            }
-         }
-         return static_cast<int>(std::round(end));
+        int left=1;
+        int right=x;
+        while(left<=right){
+            long long mid= (left) + (right-left)/2;
+            if(mid*mid==(long long)x)return (int)mid;
+            if(mid*mid>(long long )x)right= mid-1;
+            else left= mid+1;
+        }
+        return round(right);
     }
 };
