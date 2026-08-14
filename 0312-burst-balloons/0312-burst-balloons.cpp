ae2af72a1063507aa9f371249e49b79067966ac1
Ansh Tayal <tayalansh176@gmail.com>
2026-08-14 09:28:26 +0530

    Time: 91 ms (86.93%), Space: 13.8 MB (62.1%) - LeetHub

--- 0312-burst-balloons/0312-burst-balloons.cpp
+++ 0312-burst-balloons/0312-burst-balloons.cpp
@@ -1,6 +1,6 @@
 class Solution {
 public:
-    int solve(vector<int>& nums, int i, int j, vector<vector< int>>& dp) {
+    int solve(vector<int>& nums, int i, int j, vector<vector<int>>& dp) {
         if (i > j) {
             return 0;
         }
@@ -11,7 +11,7 @@ public:
         for (int k = i; k <= j; k++) {
             int cost1 = solve(nums, i, k - 1, dp);
             int cost2 = solve(nums, k + 1, j, dp);
-            int total = cost1 + cost2 + nums[i - 1] * nums[k] * nums[j+1];
+            int total = cost1 + cost2 + nums[i - 1] * nums[k] * nums[j + 1];
             max_cost = max(max_cost, total);
         }
         return dp[i][j] = max_cost;
@@ -20,7 +20,21 @@ public:
         int n = nums.size();
         nums.push_back(1);
         nums.insert(nums.begin(), 1);
-        vector<vector<int>> dp(n + 2, vector<int>(n + 2, -1));
-        return solve(nums, 1, n, dp);
+        vector<vector<int>> dp(n + 2, vector<int>(n + 2, 0));
+
+        for (int i = n; i >= 1; i--) {
+            for (int j = 1; j <=n; j++) {
+                int max_cost = 0;
+                for (int k = i; k <= j; k++) {
+                    int cost1 = dp[i][k - 1];
+                    int cost2 = dp[k + 1][j];
+                    int total =
+                        cost1 + cost2 + nums[i - 1] * nums[k] * nums[j + 1];
+                    max_cost = max(max_cost, total);
+                }
+                dp[i][j] = max_cost;
+            }
+        }
+        return dp[1][n];
     }
 };
