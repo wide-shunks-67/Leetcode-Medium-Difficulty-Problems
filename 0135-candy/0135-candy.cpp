07f041c1d183dbc594d6a48367e74e35b019e2c3
Ansh Tayal <tayalansh176@gmail.com>
2026-08-22 09:24:18 +0530

    Time: 5 ms (12.6%), Space: 71.2 MB (9.22%) - LeetHub

---
+++ 0135-candy/0135-candy.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int candy(vector<int>& ratings) {
+        int n = ratings.size();
+        
+        vector<int> arr(n, 1);
+        for (int i = 1; i < n; i++) {
+            if (ratings[i] > ratings[i-1])
+                {arr[i]=arr[i-1]+1;}
+        }
+        for (int i = n-2; i>=0; i--) {
+            if (ratings[i] > ratings[i + 1])
+                {arr[i]= max(arr[i],arr[i+1]+1);}
+        }
+        int sum=0;
+        for(int i:arr){
+            sum+=i;
+        }
+        return sum;
+    }
+};
